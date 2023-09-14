14c22bd2c7e2247c4d840c4fd9a78f19e221fcbe
t.planes <thomas.planes@isae-supaero.fr>
2023-09-14 15:42:32 +0200

    Formatting

--- aeromaps/core/process.py
+++ aeromaps/core/process.py
@@ -231,7 +231,6 @@ class AeromapsProcess(object):
                     # self.data["vector_outputs"] = self.data["vector_outputs"].merge(disc.model.df)
             self.data["float_outputs"].update(disc.model.float_outputs)
 
-
     def _update_dataframes_from_data(self):
 
         # Float parameters
