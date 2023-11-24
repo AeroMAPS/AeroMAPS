d7d70b963279c8410b8439c4ee4cac1ed4d74d72
Thomas Planès <thomas.planes@isae-supaero.fr>
2023-11-24 17:07:31 +0100

    Corrected parameters.py

--- aeromaps/models/parameters.py
+++ aeromaps/models/parameters.py
@@ -6,30 +6,26 @@ from aeromaps.utils.functions import _dict_from_json
 
 
 class Parameters:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def to_dict(self):
+        data = self.__dict__
 
-    def __repr__(self):
-        attributes = ", ".join(f"{key}={getattr(self, key)}" for key in vars(self))
-        return f"Parameters({attributes})"
-
-    def dict(self):
-        parameters_dict = self.__dict__
-
-        for key, value in parameters_dict.items():
+        for key, value in data.items():
             if isinstance(value, pd.Series):
-                parameters_dict[key] = list(value)
+                data[key] = list(value)
 
-        return parameters_dict
+        return data
+
+    def from_dict(self, data):
+        for key, value in data.items():
+            setattr(self, key, value)
 
     def write_json(self, file_name="parameters.json"):
         with open(file_name, "w", encoding="utf-8") as f:
-            dump(self.dict(), f, ignore_nan=True, ensure_ascii=False, indent=4)
+            dump(self.to_dict(), f, ignore_nan=True, ensure_ascii=False, indent=4)
 
-    @classmethod
-    def read_json(cls, file_name="parameters.json"):
+    def read_json(self, file_name="parameters.json"):
 
         data = _dict_from_json(file_name=file_name)
 
-        return cls(**data)
+        # Old reference data is kept
+        self.from_dict(data)
