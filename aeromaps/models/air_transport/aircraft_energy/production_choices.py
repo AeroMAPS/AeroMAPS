69a9d5636522323228ed0970ea953aaa8d71b8ed
Thomas Planès <thomas.planes@isae-supaero.fr>
2023-11-24 15:38:11 +0100

    Update models for decades (production choices)

--- aeromaps/models/air_transport/aircraft_energy/production_choices.py
+++ aeromaps/models/air_transport/aircraft_energy/production_choices.py
@@ -13,78 +13,84 @@ class BiofuelProduction(AeromapsModel):
 
     def compute(
         self,
-        biofuel_hefa_fog_share_2020: float,
-        biofuel_hefa_fog_share_2030: float,
-        biofuel_hefa_fog_share_2040: float,
-        biofuel_hefa_fog_share_2050: float,
-        biofuel_hefa_others_share_2020: float,
-        biofuel_hefa_others_share_2030: float,
-        biofuel_hefa_others_share_2040: float,
-        biofuel_hefa_others_share_2050: float,
-        biofuel_ft_others_share_2020: float,
-        biofuel_ft_others_share_2030: float,
-        biofuel_ft_others_share_2040: float,
-        biofuel_ft_others_share_2050: float,
-        biofuel_ft_msw_share_2020: float,
-        biofuel_ft_msw_share_2030: float,
-        biofuel_ft_msw_share_2040: float,
-        biofuel_ft_msw_share_2050: float,
+        biofuel_hefa_fog_share_reference_years: list = [],
+        biofuel_hefa_fog_share_reference_years_values: list = [],
+        biofuel_hefa_others_share_reference_years: list = [],
+        biofuel_hefa_others_share_reference_years_values: list = [],
+        biofuel_ft_others_share_reference_years: list = [],
+        biofuel_ft_others_share_reference_years_values: list = [],
+        biofuel_ft_msw_share_reference_years: list = [],
+        biofuel_ft_msw_share_reference_years_values: list = [],
     ) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series, pd.Series]:
         """Biomass production calculation using interpolation functions"""
 
-        reference_years = [2020, 2030, 2040, self.parameters.end_year]
-
         # HEFA FOG
-        reference_values_hefa_fog = [
-            biofuel_hefa_fog_share_2020,
-            biofuel_hefa_fog_share_2030,
-            biofuel_hefa_fog_share_2040,
-            biofuel_hefa_fog_share_2050,
-        ]
-        biofuel_hefa_fog_share_function = interp1d(
-            reference_years, reference_values_hefa_fog, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "biofuel_hefa_fog_share"] = biofuel_hefa_fog_share_function(k)
+        if len(biofuel_hefa_fog_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_hefa_fog_share"
+                ] = biofuel_hefa_fog_share_reference_years_values
+        else:
+            biofuel_hefa_fog_share_function = interp1d(
+                biofuel_hefa_fog_share_reference_years,
+                biofuel_hefa_fog_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_hefa_fog_share"
+                ] = biofuel_hefa_fog_share_function(k)
 
         # HEFA OTHERS
-        reference_values_hefa_others = [
-            biofuel_hefa_others_share_2020,
-            biofuel_hefa_others_share_2030,
-            biofuel_hefa_others_share_2040,
-            biofuel_hefa_others_share_2050,
-        ]
-        biofuel_hefa_others_share_function = interp1d(
-            reference_years, reference_values_hefa_others, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "biofuel_hefa_others_share"] = biofuel_hefa_others_share_function(k)
+        if len(biofuel_hefa_others_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_hefa_others_share"
+                ] = biofuel_hefa_others_share_reference_years_values
+        else:
+            biofuel_hefa_others_share_function = interp1d(
+                biofuel_hefa_others_share_reference_years,
+                biofuel_hefa_others_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_hefa_others_share"
+                ] = biofuel_hefa_others_share_function(k)
 
         # FT OTHERS
-        reference_values_ft_others = [
-            biofuel_ft_others_share_2020,
-            biofuel_ft_others_share_2030,
-            biofuel_ft_others_share_2040,
-            biofuel_ft_others_share_2050,
-        ]
-        biofuel_ft_others_share_function = interp1d(
-            reference_years, reference_values_ft_others, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "biofuel_ft_others_share"] = biofuel_ft_others_share_function(k)
+        if len(biofuel_ft_others_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_ft_others_share"
+                ] = biofuel_ft_others_share_reference_years_values
+        else:
+            biofuel_ft_others_share_function = interp1d(
+                biofuel_ft_others_share_reference_years,
+                biofuel_ft_others_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_ft_others_share"
+                ] = biofuel_ft_others_share_function(k)
 
         # FT MSW
-        reference_values_ft_msw = [
-            biofuel_ft_msw_share_2020,
-            biofuel_ft_msw_share_2030,
-            biofuel_ft_msw_share_2040,
-            biofuel_ft_msw_share_2050,
-        ]
-        biofuel_ft_msw_share_function = interp1d(
-            reference_years, reference_values_ft_msw, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "biofuel_ft_msw_share"] = biofuel_ft_msw_share_function(k)
+        if len(biofuel_ft_msw_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_ft_msw_share"
+                ] = biofuel_ft_msw_share_reference_years_values
+        else:
+            biofuel_ft_msw_share_function = interp1d(
+                biofuel_ft_msw_share_reference_years,
+                biofuel_ft_msw_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "biofuel_ft_msw_share"
+                ] = biofuel_ft_msw_share_function(k)
 
         biofuel_hefa_fog_share = self.df["biofuel_hefa_fog_share"]
         biofuel_hefa_others_share = self.df["biofuel_hefa_others_share"]
@@ -116,76 +122,84 @@ class HydrogenProduction(AeromapsModel):
 
     def compute(
         self,
-        hydrogen_electrolysis_share_2020: float = 0.0,
-        hydrogen_electrolysis_share_2030: float = 0.0,
-        hydrogen_electrolysis_share_2040: float = 0.0,
-        hydrogen_electrolysis_share_2050: float = 0.0,
-        hydrogen_gas_ccs_share_2020: float = 0.0,
-        hydrogen_gas_ccs_share_2030: float = 0.0,
-        hydrogen_gas_ccs_share_2040: float = 0.0,
-        hydrogen_gas_ccs_share_2050: float = 0.0,
-        hydrogen_coal_ccs_share_2020: float = 0.0,
-        hydrogen_coal_ccs_share_2030: float = 0.0,
-        hydrogen_coal_ccs_share_2040: float = 0.0,
-        hydrogen_coal_ccs_share_2050: float = 0.0,
-        hydrogen_gas_share_2020: float = 0.0,
-        hydrogen_gas_share_2030: float = 0.0,
-        hydrogen_gas_share_2040: float = 0.0,
-        hydrogen_gas_share_2050: float = 0.0,
+        hydrogen_electrolysis_share_reference_years: list = [],
+        hydrogen_electrolysis_share_reference_years_values: list = [],
+        hydrogen_gas_ccs_share_reference_years: list = [],
+        hydrogen_gas_ccs_share_reference_years_values: list = [],
+        hydrogen_coal_ccs_share_reference_years: list = [],
+        hydrogen_coal_ccs_share_reference_years_values: list = [],
+        hydrogen_gas_share_reference_years: list = [],
+        hydrogen_gas_share_reference_years_values: list = [],
     ) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series, pd.Series]:
         """Hydrogen production calculation using interpolation functions"""
 
-        reference_years = [2020, 2030, 2040, self.parameters.end_year]
-
         # Electrolysis
-        reference_values_electrolysis = [
-            hydrogen_electrolysis_share_2020,
-            hydrogen_electrolysis_share_2030,
-            hydrogen_electrolysis_share_2040,
-            hydrogen_electrolysis_share_2050,
-        ]
-        hydrogen_electrolysis_share_function = interp1d(
-            reference_years, reference_values_electrolysis, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "hydrogen_electrolysis_share"] = hydrogen_electrolysis_share_function(k)
+        if len(hydrogen_electrolysis_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_electrolysis_share"
+                ] = hydrogen_electrolysis_share_reference_years_values
+        else:
+            hydrogen_electrolysis_share_function = interp1d(
+                hydrogen_electrolysis_share_reference_years,
+                hydrogen_electrolysis_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_electrolysis_share"
+                ] = hydrogen_electrolysis_share_function(k)
 
         # Gas CCS
-        reference_values_gas_ccs = [
-            hydrogen_gas_ccs_share_2020,
-            hydrogen_gas_ccs_share_2030,
-            hydrogen_gas_ccs_share_2040,
-            hydrogen_gas_ccs_share_2050,
-        ]
-        hydrogen_gas_ccs_share_function = interp1d(
-            reference_years, reference_values_gas_ccs, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "hydrogen_gas_ccs_share"] = hydrogen_gas_ccs_share_function(k)
+        if len(hydrogen_gas_ccs_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_gas_ccs_share"
+                ] = hydrogen_gas_ccs_share_reference_years_values
+        else:
+            hydrogen_gas_ccs_share_function = interp1d(
+                hydrogen_gas_ccs_share_reference_years,
+                hydrogen_gas_ccs_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_gas_ccs_share"
+                ] = hydrogen_gas_ccs_share_function(k)
 
         # Coal CCS
-        reference_values_coal_ccs = [
-            hydrogen_coal_ccs_share_2020,
-            hydrogen_coal_ccs_share_2030,
-            hydrogen_coal_ccs_share_2040,
-            hydrogen_coal_ccs_share_2050,
-        ]
-        hydrogen_coal_ccs_share_function = interp1d(
-            reference_years, reference_values_coal_ccs, kind="linear"
-        )
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "hydrogen_coal_ccs_share"] = hydrogen_coal_ccs_share_function(k)
+        if len(hydrogen_coal_ccs_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_coal_ccs_share"
+                ] = hydrogen_coal_ccs_share_reference_years_values
+        else:
+            hydrogen_coal_ccs_share_function = interp1d(
+                hydrogen_coal_ccs_share_reference_years,
+                hydrogen_coal_ccs_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_coal_ccs_share"
+                ] = hydrogen_coal_ccs_share_function(k)
 
         # Gas
-        reference_values_gas = [
-            hydrogen_gas_share_2020,
-            hydrogen_gas_share_2030,
-            hydrogen_gas_share_2040,
-            hydrogen_gas_share_2050,
-        ]
-        hydrogen_gas_share_function = interp1d(reference_years, reference_values_gas, kind="linear")
-        for k in range(self.prospection_start_year, self.end_year + 1):
-            self.df.loc[k, "hydrogen_gas_share"] = hydrogen_gas_share_function(k)
+        if len(hydrogen_gas_share_reference_years) == 0:
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_gas_share"
+                ] = hydrogen_gas_share_reference_years_values
+        else:
+            hydrogen_gas_share_function = interp1d(
+                hydrogen_gas_share_reference_years,
+                hydrogen_gas_share_reference_years_values,
+                kind="linear",
+            )
+            for k in range(self.prospection_start_year, self.end_year + 1):
+                self.df.loc[
+                    k, "hydrogen_gas_share"
+                ] = hydrogen_gas_share_function(k)
 
         hydrogen_electrolysis_share = self.df["hydrogen_electrolysis_share"]
         hydrogen_gas_ccs_share = self.df["hydrogen_gas_ccs_share"]
