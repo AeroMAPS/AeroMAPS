74adb05a8d7cd46af19bfea84e8a373733f6a001
Thomas Planès <thomas.planes@isae-supaero.fr>
2023-11-30 22:51:37 +0100

    Add generic models for fuel distribution including constraint on positive values

--- aeromaps/models/air_transport/aircraft_energy/fuel_distribution.py
+++ aeromaps/models/air_transport/aircraft_energy/fuel_distribution.py
@@ -1,9 +1,8 @@
 from typing import Tuple
 
 import pandas as pd
-from scipy.interpolate import interp1d
 
-from aeromaps.models.base import AeromapsModel
+from aeromaps.models.base import AeromapsModel, InterpolationAeromapsFunction
 
 
 class DropinFuelDistribution(AeromapsModel):
@@ -20,41 +19,33 @@ class DropinFuelDistribution(AeromapsModel):
         """Fuel distribution calculation using interpolation functions"""
 
         # Biofuel
+        biofuel_share_prospective = InterpolationAeromapsFunction(
+            self,
+            biofuel_share_reference_years,
+            biofuel_share_reference_years_values,
+            method="quadratic",
+            positive_constraint=True
+        )
+        self.df.loc[:, "biofuel_share"] = biofuel_share_prospective
         for k in range(self.historic_start_year, self.prospection_start_year):
-            self.df.loc[k, "biofuel_share"] = 0
-        if len(biofuel_share_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "biofuel_share"] = biofuel_share_reference_years_values
-        else:
-            biofuel_share_function = interp1d(
-                biofuel_share_reference_years,
-                biofuel_share_reference_years_values,
-                kind="quadratic",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                if biofuel_share_function(k) <= 0:
-                    self.df.loc[k, "biofuel_share"] = 0
-                else:
-                    self.df.loc[k, "biofuel_share"] = biofuel_share_function(k)
+            self.df.loc[k, "biofuel_share"] = self.df.loc[
+                self.prospection_start_year, "biofuel_share"
+            ]
         biofuel_share = self.df["biofuel_share"]
 
         # Electrofuel
+        electrofuel_share_prospective = InterpolationAeromapsFunction(
+            self,
+            electrofuel_share_reference_years,
+            electrofuel_share_reference_years_values,
+            method="quadratic",
+            positive_constraint=True
+        )
+        self.df.loc[:, "electrofuel_share"] = electrofuel_share_prospective
         for k in range(self.historic_start_year, self.prospection_start_year):
-            self.df.loc[k, "electrofuel_share"] = 0
-        if len(electrofuel_share_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "electrofuel_share"] = electrofuel_share_reference_years_values
-        else:
-            electrofuel_share_function = interp1d(
-                electrofuel_share_reference_years,
-                electrofuel_share_reference_years_values,
-                kind="quadratic",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                if electrofuel_share_function(k) <= 0:
-                    self.df.loc[k, "electrofuel_share"] = 0
-                else:
-                    self.df.loc[k, "electrofuel_share"] = electrofuel_share_function(k)
+            self.df.loc[k, "electrofuel_share"] = self.df.loc[
+                self.prospection_start_year, "electrofuel_share"
+            ]
         electrofuel_share = self.df["electrofuel_share"]
 
         # Kerosene
