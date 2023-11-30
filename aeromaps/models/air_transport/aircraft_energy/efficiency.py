36fcfa5359a719620dccf4a3d034cd77f9d7ccd3
Thomas Planès <thomas.planes@isae-supaero.fr>
2023-11-30 17:15:28 +0100

    Update models with new generic function

--- aeromaps/models/air_transport/aircraft_energy/efficiency.py
+++ aeromaps/models/air_transport/aircraft_energy/efficiency.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
-from aeromaps.models.base import AeromapsModel
+from aeromaps.models.base import AeromapsModel, InterpolationAeromapsFunction
 
 
 class BiofuelEfficiency(AeromapsModel):
@@ -25,73 +25,28 @@ class BiofuelEfficiency(AeromapsModel):
         """Biofuel production efficiency calculation using interpolation functions"""
 
         # FT
-        if len(biofuel_ft_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_ft_efficiency"
-                ] = biofuel_ft_efficiency_reference_years_values
-        else:
-            biofuel_ft_efficiency_function = interp1d(
-                biofuel_ft_efficiency_reference_years,
-                biofuel_ft_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "biofuel_ft_efficiency"] = biofuel_ft_efficiency_function(k)
+        biofuel_ft_efficiency = InterpolationAeromapsFunction(
+            self, biofuel_ft_efficiency_reference_years, biofuel_ft_efficiency_reference_years_values
+        )
+        self.df.loc[:, "biofuel_ft_efficiency"] = biofuel_ft_efficiency
 
         # ATJ
-        if len(biofuel_atj_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_atj_efficiency"
-                ] = biofuel_atj_efficiency_reference_years_values
-        else:
-            biofuel_atj_efficiency_function = interp1d(
-                biofuel_atj_efficiency_reference_years,
-                biofuel_atj_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "biofuel_atj_efficiency"] = biofuel_atj_efficiency_function(k)
+        biofuel_atj_efficiency = InterpolationAeromapsFunction(
+            self, biofuel_atj_efficiency_reference_years, biofuel_atj_efficiency_reference_years_values
+        )
+        self.df.loc[:, "biofuel_atj_efficiency"] = biofuel_atj_efficiency
 
         # HEFA OIL
-        if len(biofuel_hefa_oil_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_hefa_oil_efficiency"
-                ] = biofuel_hefa_oil_efficiency_reference_years_values
-        else:
-            biofuel_hefa_oil_efficiency_function = interp1d(
-                biofuel_hefa_oil_efficiency_reference_years,
-                biofuel_hefa_oil_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_hefa_oil_efficiency"
-                ] = biofuel_hefa_oil_efficiency_function(k)
+        biofuel_hefa_oil_efficiency = InterpolationAeromapsFunction(
+            self, biofuel_hefa_oil_efficiency_reference_years, biofuel_hefa_oil_efficiency_reference_years_values
+        )
+        self.df.loc[:, "biofuel_hefa_oil_efficiency"] = biofuel_hefa_oil_efficiency
 
         # HEFA FUEL
-        if len(biofuel_hefa_fuel_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_hefa_fuel_efficiency"
-                ] = biofuel_hefa_fuel_efficiency_reference_years_values
-        else:
-            biofuel_hefa_fuel_efficiency_function = interp1d(
-                biofuel_hefa_fuel_efficiency_reference_years,
-                biofuel_hefa_fuel_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "biofuel_hefa_fuel_efficiency"
-                ] = biofuel_hefa_fuel_efficiency_function(k)
-
-        biofuel_ft_efficiency = self.df["biofuel_ft_efficiency"]
-        biofuel_atj_efficiency = self.df["biofuel_atj_efficiency"]
-        biofuel_hefa_oil_efficiency = self.df["biofuel_hefa_oil_efficiency"]
-        biofuel_hefa_fuel_efficiency = self.df["biofuel_hefa_fuel_efficiency"]
+        biofuel_hefa_fuel_efficiency = InterpolationAeromapsFunction(
+            self, biofuel_hefa_fuel_efficiency_reference_years, biofuel_hefa_fuel_efficiency_reference_years_values
+        )
+        self.df.loc[:, "biofuel_hefa_fuel_efficiency"] = biofuel_hefa_fuel_efficiency
 
         return (
             biofuel_ft_efficiency,
@@ -117,54 +72,21 @@ class ElectricityBasedFuelEfficiency(AeromapsModel):
         """Hydrogen and electrofuel production efficiency calculation using interpolation functions"""
 
         # Electrolysis
-        if len(electrolysis_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "electrolysis_efficiency"
-                ] = electrolysis_efficiency_reference_years_values
-        else:
-            electrolysis_efficiency_function = interp1d(
-                electrolysis_efficiency_reference_years,
-                electrolysis_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "electrolysis_efficiency"] = electrolysis_efficiency_function(k)
+        electrolysis_efficiency = InterpolationAeromapsFunction(
+            self, electrolysis_efficiency_reference_years, electrolysis_efficiency_reference_years_values
+        )
+        self.df.loc[:, "electrolysis_efficiency"] = electrolysis_efficiency
 
         # Liquefaction
-        if len(liquefaction_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "liquefaction_efficiency"
-                ] = liquefaction_efficiency_reference_years_values
-        else:
-            liquefaction_efficiency_function = interp1d(
-                liquefaction_efficiency_reference_years,
-                liquefaction_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[k, "liquefaction_efficiency"] = liquefaction_efficiency_function(k)
+        liquefaction_efficiency = InterpolationAeromapsFunction(
+            self, liquefaction_efficiency_reference_years, liquefaction_efficiency_reference_years_values
+        )
+        self.df.loc[:, "liquefaction_efficiency"] = liquefaction_efficiency
 
         # Electrofuel from hydrogen
-        if len(electrofuel_hydrogen_efficiency_reference_years) == 0:
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "electrofuel_hydrogen_efficiency"
-                ] = electrofuel_hydrogen_efficiency_reference_years_values
-        else:
-            electrofuel_hydrogen_efficiency_function = interp1d(
-                electrofuel_hydrogen_efficiency_reference_years,
-                electrofuel_hydrogen_efficiency_reference_years_values,
-                kind="linear",
-            )
-            for k in range(self.prospection_start_year, self.end_year + 1):
-                self.df.loc[
-                    k, "electrofuel_hydrogen_efficiency"
-                ] = electrofuel_hydrogen_efficiency_function(k)
-
-        electrolysis_efficiency = self.df["electrolysis_efficiency"]
-        liquefaction_efficiency = self.df["liquefaction_efficiency"]
-        electrofuel_hydrogen_efficiency = self.df["electrofuel_hydrogen_efficiency"]
+        electrofuel_hydrogen_efficiency = InterpolationAeromapsFunction(
+            self, electrofuel_hydrogen_efficiency_reference_years, electrofuel_hydrogen_efficiency_reference_years_values
+        )
+        self.df.loc[:, "electrofuel_hydrogen_efficiency"] = electrofuel_hydrogen_efficiency
 
         return electrolysis_efficiency, liquefaction_efficiency, electrofuel_hydrogen_efficiency
