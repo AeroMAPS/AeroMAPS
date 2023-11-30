5b6c84028db1d75cd6e1b2d30fd6ff545057c2d3
Thomas Planès <thomas.planes@isae-supaero.fr>
2023-11-30 21:47:43 +0100

    Temporary solution for fleet model warnings

--- aeromaps/models/air_transport/aircraft_fleet_and_operations/fleet/fleet_model.py
+++ aeromaps/models/air_transport/aircraft_fleet_and_operations/fleet/fleet_model.py
@@ -65,6 +65,10 @@ class FleetModel(AeromapsModel):
     def compute(
         self,
     ):
+
+        # TODO : correct warnings
+        warnings.filterwarnings("ignore")
+
         # Start from empty dataframe
         self.df = self.df.filter([])
 
@@ -91,6 +95,9 @@ class FleetModel(AeromapsModel):
 
         # Compute mean non-CO2 emission index per category with respect to energy type
         self._compute_mean_non_co2_emission_index()
+
+        warnings.resetwarnings()
+        warnings.simplefilter("ignore", DeprecationWarning)
 
     def _compute_energy_consumption_and_share_wrt_energy_type(self):
         # Energy consumption calculations for drop-in fuel and hydrogen
@@ -809,8 +816,7 @@ class FleetModel(AeromapsModel):
                 )
 
     def _compute_mean_non_co2_emission_index(self):
-        # TODO : correct warnings
-        warnings.filterwarnings("ignore")
+
         for category in self.fleet.categories.values():
             # Mean non-CO2 emission index per category
             # Initialization
@@ -889,8 +895,7 @@ class FleetModel(AeromapsModel):
                 ] * (
                     self.df.loc[k, category.name + ":share:hydrogen"] / 100
                 )
-        warnings.resetwarnings()
-        warnings.simplefilter("ignore", DeprecationWarning)
+
 
     def _compute_aircraft_share(self):
         # Aircraft share computation
