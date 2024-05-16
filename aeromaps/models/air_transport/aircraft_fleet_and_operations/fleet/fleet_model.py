6a53ef40db0bbf5b7a65ce88c4a5a6e1634e7463
antSGS <97613437+antSGS@users.noreply.github.com>
2024-05-16 13:28:51 +0200

    temporary fix for issue in GUI

--- aeromaps/models/air_transport/aircraft_fleet_and_operations/fleet/fleet_model.py
+++ aeromaps/models/air_transport/aircraft_fleet_and_operations/fleet/fleet_model.py
@@ -1894,10 +1894,12 @@ class Fleet(object):
         sr_nb_cat.recent_reference_aircraft.rc_cost = 40000000.0
         sr_nb_cat.recent_reference_aircraft.nrc_cost = 10000000000.0
 
+        #TODO CAUTION energy_consumption_init[19] is a weak fix => type of the variable changes if multiple runs! Linked to Utils/function.py?
+
         mean_energy_init_ask_short_range = (
-            self.parameters.energy_consumption_init.values[-1]
+            self.parameters.energy_consumption_init[19]
             * self.parameters.short_range_energy_share_2019
-        ) / (self.parameters.ask_init.values[-1] * self.parameters.short_range_rpk_share_2019)
+        ) / (self.parameters.ask_init[19] * self.parameters.short_range_rpk_share_2019)
 
         share_recent_short_range = (
             mean_energy_init_ask_short_range - sr_nb_cat.old_reference_aircraft.energy_per_ask
@@ -2018,9 +2020,9 @@ class Fleet(object):
         mr_subcat.recent_reference_aircraft.nrc_cost = 10000000000.0
 
         mean_energy_init_ask_medium_range = (
-            self.parameters.energy_consumption_init.values[-1]
+            self.parameters.energy_consumption_init[19]
             * self.parameters.medium_range_energy_share_2019
-        ) / (self.parameters.ask_init.values[-1] * self.parameters.medium_range_rpk_share_2019)
+        ) / (self.parameters.ask_init[19] * self.parameters.medium_range_rpk_share_2019)
 
         share_recent_medium_range = (
             mean_energy_init_ask_medium_range - mr_subcat.old_reference_aircraft.energy_per_ask
@@ -2082,9 +2084,9 @@ class Fleet(object):
         lr_subcat.recent_reference_aircraft.nrc_cost = 25000000000.0
 
         mean_energy_init_ask_long_range = (
-            self.parameters.energy_consumption_init.values[-1]
+            self.parameters.energy_consumption_init[19]
             * self.parameters.long_range_energy_share_2019
-        ) / (self.parameters.ask_init.values[-1] * self.parameters.long_range_rpk_share_2019)
+        ) / (self.parameters.ask_init[19] * self.parameters.long_range_rpk_share_2019)
 
         share_recent_long_range = (
             mean_energy_init_ask_long_range - lr_subcat.old_reference_aircraft.energy_per_ask
