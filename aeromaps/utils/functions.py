19225d5921c30b567749034aba3ebbda8e28e8e1
Scott Delbecq <scott.delbecq91@gmail.com>
2025-06-25 13:23:03 +0200

    Added merge json function

--- aeromaps/utils/functions.py
+++ aeromaps/utils/functions.py
@@ -246,6 +246,15 @@ def create_partitioning(file, path=""):
 
     return
 
+def merge_json_files(file1, file2, output_file):
+    with open(file1, "r") as f1, open(file2, "r") as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    merged_data = {**data1, **data2}
+
+    with open(output_file, "w") as outfile:
+        json.dump(merged_data, outfile, indent=4)
 
 def compare_json_files(
     file1_path: str,
