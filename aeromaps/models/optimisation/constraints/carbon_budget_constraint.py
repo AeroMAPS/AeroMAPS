89f193b7fa10affce387cc4682e4c2eccffd8638
a.salgas <antoine732@hotmail.fr>
2025-10-02 14:08:07 +0200

    Addressed a potential division by zero in carbon constraint

--- aeromaps/models/optimisation/constraints/carbon_budget_constraint.py
+++ aeromaps/models/optimisation/constraints/carbon_budget_constraint.py
@@ -2,6 +2,8 @@
 # @Author : a.salgas
 # @File : carbon_budget_constraint.py
 # @Software: PyCharm
+import logging
+
 import pandas as pd
 
 from aeromaps.models.base import AeroMAPSModel
@@ -25,6 +27,18 @@ class CarbonBudgetConstraint(AeroMAPSModel):
             gross_carbon_budget_2050 * aviation_carbon_budget_objective / 100
             - cumulative_co2_emissions.loc[2025]
         )
+
+        # avoid division by 0: if adjusted carbon budget is lower <= 0, impossible to decarbonise:
+        # trigger infeasibility in the optimisation
+        print(aviation_carbon_budget_objective, cumulative_co2_emissions.loc[2025])
+        if adjusted_carbon_budget_2050 <= 0:
+            logging.warning(
+                "Adjusted carbon budget for aviation in 2050 is <= 0. "
+                "This indicates that the cumulative emissions up to 2025 have already exceeded "
+                "the allocated carbon budget for aviation. "
+                "The optimisation problem may be infeasible."
+            )
+            adjusted_carbon_budget_2050 = 1e6  # small positive number
 
         aviation_carbon_budget_constraint = (
             +(cumulative_emissions - adjusted_carbon_budget_2050) / adjusted_carbon_budget_2050
