354c12e32ebcd0fade7e92de7d339a6c728194da
a.salgas <antoine732@hotmail.fr>
2025-11-14 16:30:21 +0100

    Enhance Sphinx configuration for improved documentation generation

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,21 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+# Make the project importable by Sphinx (important for autodoc)
+
+import os
+import sys
+
+# Adjust depending on your project structure:
+# Here we assume:
+#   AeroMAPS/
+#     aeromaps/    <-- your package
+#     docs/
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,14 +29,40 @@ release = "v0.9.0-beta"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Autosummary: generate stub files automatically
+autosummary_generate = True
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Render `None` as “None” instead of type NoneType
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+# Show type hints in the description, not in the function signature
+set_type_checking_flag = True
+typehints_fully_qualified = False
+
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# Use the Read the Docs theme (cleaner and more standard for Sphinx projects)
+html_theme = "sphinx_rtd_theme"
+
 html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,35 @@
-.. AeroMAPS documentation master file, created by
-   sphinx-quickstart on Fri Nov 14 16:14:52 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 AeroMAPS documentation
 ======================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+Welcome to the AeroMAPS documentation!
 
+AeroMAAPS description to be completed.
+
+Contents
+--------
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+
+   api                 # API reference folder (we’ll generate it)
+
+---
+
+API Reference
+-------------
+
+The following modules are documented automatically:
+
+.. autosummary::
+   :toctree: api
+   :caption: AeroMAPS API
+   :recursive:
+
+   aeromaps.core
+   aeromaps.gui
+   aeromaps.models
+   aeromaps.notebooks
+   aeromaps.plots
+   aeromaps.utils
 
