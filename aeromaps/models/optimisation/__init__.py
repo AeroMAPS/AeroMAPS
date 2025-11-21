9d7c8cb5efb7bca7da03a5ab919e6d180207d623
a.salgas <antoine732@hotmail.fr>
2025-11-21 17:25:41 +0100

    Docstring finished for optim constraints module.

--- aeromaps/models/optimisation/__init__.py
+++ aeromaps/models/optimisation/__init__.py
@@ -2,3 +2,9 @@
 # @Author : a.salgas
 # @File : __init__.py.py
 # @Software: PyCharm
+"""
+optimisation
+
+===============
+This package contains models related to optimisation processes within the AeroMAPS framework.
+"""
